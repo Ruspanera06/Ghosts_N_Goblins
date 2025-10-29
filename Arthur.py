70627848569734ab279e0c75f99b673ff7ae2064
Ruspanera06 <harold.boado2006@gmail.com>
2025-10-29 13:21:14 +0100

    fixed zoom

--- Arthur.py
+++ Arthur.py
@@ -8,6 +8,7 @@ class Arthur(Actor):
         self.max_jump = 40
         self._djump = 4
         self._speed = 2
+        self._health = 3
 
     def move(self, arena: Arena):
         G = 5
@@ -16,18 +17,22 @@ class Arthur(Actor):
         #         self.hit(arena)
 
         keys = arena.current_keys()
-        if "ArrowUp" in keys and self._y + self._h == arena.size()[1]:
+        if ("Spacebar" in keys or "w" in keys) and self._y + self._h == arena.size()[1] :
             self._jump = True
             self._dy -= self._djump
         elif self._y + self._h == arena.size()[1]:
             self._dy = 0
             self._jump = False
-        if "ArrowLeft" in keys:
+        if "a" in keys:
             self._x -= self._speed
-        elif "ArrowRight" in keys:
+        elif "d" in keys:
             self._x += self._speed
         
         #self._x += self._dx
+        aw, ah = arena.size()
+        self._x = min(max(self._x, 5), aw - self._w)  # clamp
+        self._y = min(max(self._y, 5), ah - self._h)  # clamp
+
         if self._y > arena.size()[1]-self.max_jump and self._jump == True:
             self._y += self._dy
         else:
@@ -35,10 +40,6 @@ class Arthur(Actor):
         if self._y + self._h < arena.size()[1] and self._jump == False:
             self._y += G
         
-
-        aw, ah = arena.size()
-        self._x = min(max(self._x, 0), aw - self._w)  # clamp
-        self._y = min(max(self._y, 0), ah - self._h)  # clamp
 
     def hit(self, arena: Arena):
         arena.kill(self)
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Arthur import Arthur
 from math import cos, sin, radians
 
 
-x_view,y_view = 100,100
-w_view, h_view = 200,150
+x_view,y_view = 300,200
+w_view, h_view = 400,400
 def tick():
     g2d.clear_canvas()
-    global x_view,y_view
+    global x_view,y_view, w_view, h_view
     g2d.draw_image("https://fondinfo.github.io/sprites/ghosts-goblins-bg.png", (0,0),(x_view, y_view),(w_view,h_view) )
     for a in arena.actors():
         if a.sprite() != None:
@@ -18,16 +18,15 @@ def tick():
             pass  # g2d.draw_rect(a.pos(), a.size())
 
     arena.tick(g2d.current_keys())  # Game logic
-    
     if "ArrowUp" in g2d.current_keys():
-        y_view -= 5
+        y_view = max(y_view-5, 0)
     elif "ArrowDown" in g2d.current_keys():
-        y_view += 5    
+        y_view = min(y_view + 5, h_view)    
         
     elif "ArrowRight" in g2d.current_keys():
-        x_view += 5
+        x_view = min(x_view + 5, w_view)
     elif "ArrowLeft" in g2d.current_keys():
-        x_view -= 5  
+        x_view = max(x_view - 5, 0)
 
 
 def main():
