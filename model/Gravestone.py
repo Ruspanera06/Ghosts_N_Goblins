924176492917b79771aac6a20a13b47cb4a8d1b8
Abdul1-1 <abdul.shahzad06@icloud.com>
2025-10-31 22:44:05 +0100

    added coords of the graves

--- model/Gravestone.py
+++ model/Gravestone.py
@@ -1,4 +1,18 @@
 from actor import Actor, Arena, Point
+IDLE = [ 
+    ((50,186),(65,201)), 
+    ((242,186),(257,201)),
+    ((418,188),(433,201)), #diffrent 
+    ((530,186),(545,201)), 
+    ((754,186),(769,201)), 
+    ((770,108),(785,121)), #platform grave diffrent 
+    ((866,106),(881,121)), #platform grave 
+    ((962,108),(977,121)), #platforn grave diffrent 
+    ((962,186),(977,201)), 
+    ((1106,186),(1121,201)), 
+    ((1266,188),(1281,201)), 
+    ((1522,186),(1537,201)) 
+]
 
 class Gravestone(Actor):
     def __init__(self, pos, pos_end):
